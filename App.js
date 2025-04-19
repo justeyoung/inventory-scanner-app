e901ccbb1eac1298fd484d9ddce7957c85d24655
justeyoung <justeyoung@yahoo.com>
2025-04-19 18:15:07 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const scriptURL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLi81o-o587EjpEZCF60eb2OEaXFIkzVzF8bacTWX-JGtGvqVls2r3rtMETMNAR3B8UARqZCxMylf-YyBHUgrGghoDQGKt8xJC_nyeuUP0HFibcfAewKbhPj6fg1ncNWoDXU1hgR2vC2dXAGcLPl_9nWu694ny74jqek4LKGlLRuu8Npal8Vx6metbhsbm3_3XAyWp5NMaYM2RF4drF2Aw6mrvu3hPerPhYlv8azTO5M4O47R4EgiFByvkQRA2Ymqs85vEAj-ivhTgcIqIW5LXKN29r7R5fWe5pzsXng&lib=MA1T5LTxm0JH1WFqDQAxrTMs6NlTRdsbA";
+const scriptURL = "https://script.google.com/macros/s/AKfycbyw911MYKuY9nM-CeaE_NtdcNewH0ohrK1NGzz0AGjbLp6E25mtA-33Ea4MU3938At5cw/exec";
                   
 async function loadInventory() {
   const res = await fetch(scriptURL);
